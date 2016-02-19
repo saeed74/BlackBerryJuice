58c093f62e50fe64578c3f202fca13e5c0fe11b9
EhCan <seyedi_es@yahoo.com>
2016-02-19 21:25:10 +0330

    Ehsan 0.2 Alpha

--- app/src/main/java/com/BlackBerryJuice/ActivitySplash.java
+++ app/src/main/java/com/BlackBerryJuice/ActivitySplash.java
@@ -2,6 +2,7 @@ package com.BlackBerryJuice;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -9,6 +10,8 @@ import android.view.Window;
 import android.view.WindowManager;
 
 public class ActivitySplash extends Activity {
+
+	public static Typeface F1,F2,F3,F4,F5,F6;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,13 @@ public class ActivitySplash extends Activity {
         getWindow().requestFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.splash);
+
+		F1=Typeface.createFromAsset(getAssets(),"fonts/IRANSansMobile_Light.ttf");
+		F2=Typeface.createFromAsset(getAssets(),"fonts/IRANSansMobile_Medium.ttf");
+		F3=Typeface.createFromAsset(getAssets(),"fonts/IRANSansMobile_Bold.ttf");
+		F4=Typeface.createFromAsset(getAssets(),"fonts/IRANSansMobile_UltraLight.ttf");
+		F5=Typeface.createFromAsset(getAssets(),"fonts/IRANSansMobile.ttf");
+		F6=Typeface.createFromAsset(getAssets(),"fonts/IRANSansMobile_Light_Persian_Digits.ttf");
         
         new CountDownTimer(5000,1000) {
         	
