d045dd60f203a5860e83bca8e58c9ccfe06541e2
Saeed <saeed.dc2011@gmail.com>
2016-02-22 16:08:39 +0330

    gallery white background

--- app/src/main/java/com/BlackBerryJuice/adapters/FullScreenImageGalleryAdapter.java
+++ app/src/main/java/com/BlackBerryJuice/adapters/FullScreenImageGalleryAdapter.java
@@ -62,9 +62,9 @@ public class FullScreenImageGalleryAdapter extends PagerAdapter {
 
                         @Override
                         public void onSuccess(Palette palette) {
-                            int bgColor = getBackgroundColor(palette);
-                            if (bgColor != -1)
-                                linearLayout.setBackgroundColor(bgColor);
+//                            int bgColor = getBackgroundColor(palette);
+//                            if (bgColor != -1)
+//                                linearLayout.setBackgroundColor(bgColor);
                         }
                     });
         } else {
@@ -93,115 +93,115 @@ public class FullScreenImageGalleryAdapter extends PagerAdapter {
     }
 
     // region Helper Methods
-    private int getBackgroundColor(Palette palette) {
-        int bgColor = -1;
-
-        int vibrantColor = palette.getVibrantColor(0x000000);
-        int lightVibrantColor = palette.getLightVibrantColor(0x000000);
-        int darkVibrantColor = palette.getDarkVibrantColor(0x000000);
-
-        int mutedColor = palette.getMutedColor(0x000000);
-        int lightMutedColor = palette.getLightMutedColor(0x000000);
-        int darkMutedColor = palette.getDarkMutedColor(0x000000);
-
-        if (mPaletteColorType != null) {
-            switch (mPaletteColorType) {
-                case VIBRANT:
-                    if (vibrantColor != 0) { // primary option
-                        bgColor = vibrantColor;
-                    } else if (lightVibrantColor != 0) { // fallback options
-                        bgColor = lightVibrantColor;
-                    } else if (darkVibrantColor != 0) {
-                        bgColor = darkVibrantColor;
-                    } else if (mutedColor != 0) {
-                        bgColor = mutedColor;
-                    } else if (lightMutedColor != 0) {
-                        bgColor = lightMutedColor;
-                    } else if (darkMutedColor != 0) {
-                        bgColor = darkMutedColor;
-                    }
-                    break;
-                case LIGHT_VIBRANT:
-                    if (lightVibrantColor != 0) { // primary option
-                        bgColor = lightVibrantColor;
-                    } else if (vibrantColor != 0) { // fallback options
-                        bgColor = vibrantColor;
-                    } else if (darkVibrantColor != 0) {
-                        bgColor = darkVibrantColor;
-                    } else if (mutedColor != 0) {
-                        bgColor = mutedColor;
-                    } else if (lightMutedColor != 0) {
-                        bgColor = lightMutedColor;
-                    } else if (darkMutedColor != 0) {
-                        bgColor = darkMutedColor;
-                    }
-                    break;
-                case DARK_VIBRANT:
-                    if (darkVibrantColor != 0) { // primary option
-                        bgColor = darkVibrantColor;
-                    } else if (vibrantColor != 0) { // fallback options
-                        bgColor = vibrantColor;
-                    } else if (lightVibrantColor != 0) {
-                        bgColor = lightVibrantColor;
-                    } else if (mutedColor != 0) {
-                        bgColor = mutedColor;
-                    } else if (lightMutedColor != 0) {
-                        bgColor = lightMutedColor;
-                    } else if (darkMutedColor != 0) {
-                        bgColor = darkMutedColor;
-                    }
-                    break;
-                case MUTED:
-                    if (mutedColor != 0) { // primary option
-                        bgColor = mutedColor;
-                    } else if (lightMutedColor != 0) { // fallback options
-                        bgColor = lightMutedColor;
-                    } else if (darkMutedColor != 0) {
-                        bgColor = darkMutedColor;
-                    } else if (vibrantColor != 0) {
-                        bgColor = vibrantColor;
-                    } else if (lightVibrantColor != 0) {
-                        bgColor = lightVibrantColor;
-                    } else if (darkVibrantColor != 0) {
-                        bgColor = darkVibrantColor;
-                    }
-                    break;
-                case LIGHT_MUTED:
-                    if (lightMutedColor != 0) { // primary option
-                        bgColor = lightMutedColor;
-                    } else if (mutedColor != 0) { // fallback options
-                        bgColor = mutedColor;
-                    } else if (darkMutedColor != 0) {
-                        bgColor = darkMutedColor;
-                    } else if (vibrantColor != 0) {
-                        bgColor = vibrantColor;
-                    } else if (lightVibrantColor != 0) {
-                        bgColor = lightVibrantColor;
-                    } else if (darkVibrantColor != 0) {
-                        bgColor = darkVibrantColor;
-                    }
-                    break;
-                case DARK_MUTED:
-                    if (darkMutedColor != 0) { // primary option
-                        bgColor = darkMutedColor;
-                    } else if (mutedColor != 0) { // fallback options
-                        bgColor = mutedColor;
-                    } else if (lightMutedColor != 0) {
-                        bgColor = lightMutedColor;
-                    } else if (vibrantColor != 0) {
-                        bgColor = vibrantColor;
-                    } else if (lightVibrantColor != 0) {
-                        bgColor = lightVibrantColor;
-                    } else if (darkVibrantColor != 0) {
-                        bgColor = darkVibrantColor;
-                    }
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        return bgColor;
-    }
+//    private int getBackgroundColor(Palette palette) {
+//        int bgColor = -1;
+//
+//        int vibrantColor = palette.getVibrantColor(0x000000);
+//        int lightVibrantColor = palette.getLightVibrantColor(0x000000);
+//        int darkVibrantColor = palette.getDarkVibrantColor(0x000000);
+//
+//        int mutedColor = palette.getMutedColor(0x000000);
+//        int lightMutedColor = palette.getLightMutedColor(0x000000);
+//        int darkMutedColor = palette.getDarkMutedColor(0x000000);
+//
+//        if (mPaletteColorType != null) {
+//            switch (mPaletteColorType) {
+//                case VIBRANT:
+//                    if (vibrantColor != 0) { // primary option
+//                        bgColor = vibrantColor;
+//                    } else if (lightVibrantColor != 0) { // fallback options
+//                        bgColor = lightVibrantColor;
+//                    } else if (darkVibrantColor != 0) {
+//                        bgColor = darkVibrantColor;
+//                    } else if (mutedColor != 0) {
+//                        bgColor = mutedColor;
+//                    } else if (lightMutedColor != 0) {
+//                        bgColor = lightMutedColor;
+//                    } else if (darkMutedColor != 0) {
+//                        bgColor = darkMutedColor;
+//                    }
+//                    break;
+//                case LIGHT_VIBRANT:
+//                    if (lightVibrantColor != 0) { // primary option
+//                        bgColor = lightVibrantColor;
+//                    } else if (vibrantColor != 0) { // fallback options
+//                        bgColor = vibrantColor;
+//                    } else if (darkVibrantColor != 0) {
+//                        bgColor = darkVibrantColor;
+//                    } else if (mutedColor != 0) {
+//                        bgColor = mutedColor;
+//                    } else if (lightMutedColor != 0) {
+//                        bgColor = lightMutedColor;
+//                    } else if (darkMutedColor != 0) {
+//                        bgColor = darkMutedColor;
+//                    }
+//                    break;
+//                case DARK_VIBRANT:
+//                    if (darkVibrantColor != 0) { // primary option
+//                        bgColor = darkVibrantColor;
+//                    } else if (vibrantColor != 0) { // fallback options
+//                        bgColor = vibrantColor;
+//                    } else if (lightVibrantColor != 0) {
+//                        bgColor = lightVibrantColor;
+//                    } else if (mutedColor != 0) {
+//                        bgColor = mutedColor;
+//                    } else if (lightMutedColor != 0) {
+//                        bgColor = lightMutedColor;
+//                    } else if (darkMutedColor != 0) {
+//                        bgColor = darkMutedColor;
+//                    }
+//                    break;
+//                case MUTED:
+//                    if (mutedColor != 0) { // primary option
+//                        bgColor = mutedColor;
+//                    } else if (lightMutedColor != 0) { // fallback options
+//                        bgColor = lightMutedColor;
+//                    } else if (darkMutedColor != 0) {
+//                        bgColor = darkMutedColor;
+//                    } else if (vibrantColor != 0) {
+//                        bgColor = vibrantColor;
+//                    } else if (lightVibrantColor != 0) {
+//                        bgColor = lightVibrantColor;
+//                    } else if (darkVibrantColor != 0) {
+//                        bgColor = darkVibrantColor;
+//                    }
+//                    break;
+//                case LIGHT_MUTED:
+//                    if (lightMutedColor != 0) { // primary option
+//                        bgColor = lightMutedColor;
+//                    } else if (mutedColor != 0) { // fallback options
+//                        bgColor = mutedColor;
+//                    } else if (darkMutedColor != 0) {
+//                        bgColor = darkMutedColor;
+//                    } else if (vibrantColor != 0) {
+//                        bgColor = vibrantColor;
+//                    } else if (lightVibrantColor != 0) {
+//                        bgColor = lightVibrantColor;
+//                    } else if (darkVibrantColor != 0) {
+//                        bgColor = darkVibrantColor;
+//                    }
+//                    break;
+//                case DARK_MUTED:
+//                    if (darkMutedColor != 0) { // primary option
+//                        bgColor = darkMutedColor;
+//                    } else if (mutedColor != 0) { // fallback options
+//                        bgColor = mutedColor;
+//                    } else if (lightMutedColor != 0) {
+//                        bgColor = lightMutedColor;
+//                    } else if (vibrantColor != 0) {
+//                        bgColor = vibrantColor;
+//                    } else if (lightVibrantColor != 0) {
+//                        bgColor = lightVibrantColor;
+//                    } else if (darkVibrantColor != 0) {
+//                        bgColor = darkVibrantColor;
+//                    }
+//                    break;
+//                default:
+//                    break;
+//            }
+//        }
+//
+//        return bgColor;
+//    }
     // endregion
 }
